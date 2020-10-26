06fa15de0a6062ec51a1f8e60fef922a29b08af8
hiratadaichi <72585462+hiratadaichi@users.noreply.github.com>
2020-10-26 21:23:49 +0900

    1981007H_HirataDaichi_kadai2-q1

--- section2-q1/sketch.js
+++ section2-q1/sketch.js
@@ -1,6 +1,13 @@
 // 小手調べ
 function setup(){
   for(let i = 0; i < 10; i++){
-    // BLANK[1]
-  }
+    if(i<5){
+   stroke(255, 0, 0);
+   ellipse(50, 70,50-i*5);
+           }
+   else{
+     stroke(0, 0, 255);
+   ellipse(50, 70,50-i*5);
+       }
+   }
 }
